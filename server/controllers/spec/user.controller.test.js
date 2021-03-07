9796a180b50f50e9eede7f8bfeaff0dfc8b1fb3f
symien moore <symien.moore@gmail.com>
2021-03-07 18:52:11 -0500

    testing with jest

---
+++ server/controllers/spec/user.controller.test.js
@@ -0,0 +1,13 @@
+const user = require('../../models/User');
+const userController = require('../user.controller');
+
+let mockUser = {
+    "name": "symi",
+    "username": "test_user",
+    "email": "test@example.com",
+    "password": "test"
+}
+
+test('should create user', () => {
+    expect(userController.registerUser(mockUser)).toBeTruthy()
+})
