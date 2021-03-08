cfccfdfd6a4ab5bce6a9133874556269c93d68a8
symien moore <symien.moore@gmail.com>
2021-03-08 14:42:26 -0500

    still working on tests

--- server/controllers/spec/post.controller.test.js
+++ server/controllers/spec/post.controller.test.js
@@ -1,5 +1,12 @@
 const postController = require('../post.controller');
 
 let mockPost = {
-    
+    title: "test",
+    description: "test",
+    body: 'test',
+    category: "networking"
 }
+
+test('should create post', () => {
+    expect(postController.addPost(mockPost)).toBeTruthy();
+})
--- server/controllers/spec/user.controller.test.js
+++ server/controllers/spec/user.controller.test.js
@@ -1,13 +1,13 @@
-// const user = require('../../models/User');
-// const userController = require('../user.controller');
+const user = require('../../models/User');
+const userController = require('../user.controller');
 
-// let mockUser = {
-//     "name": "symi",
-//     "username": "test_user",
-//     "email": "test@example.com",
-//     "password": "test"
-// }
+let mockUser = {
+    "name": "symi",
+    "username": "test_user",
+    "email": "test@example.com",
+    "password": "test"
+}
 
-// test('should create user', () => {
-//     expect(userController.registerUser(mockUser)).toBeTruthy()
-// })
+test('should create user', () => {
+    expect(userController.registerUser(mockUser)).toBeTruthy()
+})
